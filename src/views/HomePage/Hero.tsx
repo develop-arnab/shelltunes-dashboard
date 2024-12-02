2e5a8fa16586c4a9fdc2d85fd74559502421130c
develop-arnab <develop.arnab@gmail.com>
2024-12-03 00:28:39 +0530

    Handle SSO Callback

--- src/views/HomePage/Hero.tsx
+++ src/views/HomePage/Hero.tsx
@@ -1,25 +1,88 @@
 //@ts-nocheck
-import NextLink from 'next/link';
-import styled from 'styled-components';
-import Button from 'src/components/Button';
-import ButtonGroup from 'src/components/ButtonGroup';
-import Container from 'src/components/Container';
-import HeroIllustration from 'src/components/HeroIllustation';
-import OverTitle from 'src/components/OverTitle';
+import NextLink from 'next/link'
+import styled from 'styled-components'
+import Button from 'src/components/Button'
+import ButtonGroup from 'src/components/ButtonGroup'
+import Container from 'src/components/Container'
+import HeroIllustration from 'src/components/HeroIllustation'
+import OverTitle from 'src/components/OverTitle'
 import Lottie from 'react-lottie'
 // import * as animationData from '../../../public/Line_Connect_Divider_Slanted_Angle_Rotate_Color_Reveal_Stripe_Slide_Text_Animation.json'
 // import { useNewsletterModalContext } from 'src/contexts/newsletter-modal.context';
 import { media } from '../../utils/media'
 import { useRouter } from 'next/router'
-import EmblaCarousel from 'src/components/carousel/EmblaCarousel';
+import EmblaCarousel from 'src/components/carousel/EmblaCarousel'
+import { useEffect } from 'react'
+import axios from 'axios'
 // import AwesomeSlider from 'react-awesome-slider'
 // import AwesomeSliderStyles from 'react-awesome-slider/src/styles'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 export default function Hero() {
   // const { setIsModalOpened } = useNewsletterModalContext();
   const OPTIONS = { loop: true }
   const SLIDE_COUNT = 5
   const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
   const router = useRouter()
+  const { query } = router
+  const registering = false
+  // useEffect(() => {
+  //   console.log('query ', query)
+  //   console.log('router ', router.asPath)
+  //   if (router.asPath.split('&')[0].split('=')[1]) {
+  //     const router_path = router.asPath
+  //     const accessToken = router.asPath.split('&')[0].split('=')[1]
+  //     localStorage.setItem('accessToken', accessToken)
+  //   }
+  //   console.log('accessToken ', accessToken)
+  // }, [router])
+
+    useEffect(() => {
+
+      if(registering) return
+      if (router.asPath.includes('#')) {
+        const hash = router.asPath.split('#')[1] // Get the part after '#'
+        const params = new URLSearchParams(hash) // Parse it using URLSearchParams
+
+        const accessToken = params.get('access_token') // Extract access_token
+        const idToken = params.get('id_token') // Extract id_token
+
+        if (accessToken) {
+          localStorage.setItem('accessToken', accessToken)
+          console.log('Access Token:', accessToken)
+        }
+
+        if (idToken) {
+          localStorage.setItem('idToken', idToken)
+          console.log('ID Token:', idToken)
+          registering = true
+          registerLoginSSOUser(idToken)
+        }
+      }
+    }, [router])
+
+    const registerLoginSSOUser = async (idToken) => {
+      const headers = {
+        Authorization: idToken,
+        'Content-Type': 'application/json'
+      }
+
+      try {
+        const response = await axios.post(BASE_URL + '/api/authenticate-sso',{}, { headers })
+        const data = response?.data
+        console.log('DATA ', data)
+        // setSavedProjects(data)
+        console.log(data)
+        const accessToken = localStorage.getItem('accessToken')
+        if (accessToken) {
+          router.push(`${process.env.NEXT_PUBLIC_BASE_URL}/studio?token=${accessToken}`)
+          registering = false
+        }
+      } catch (error) {
+        console.error(error)
+        registering = false
+      }
+    }
+
   return (
     <HeroWrapper>
       <ImageContainer>
@@ -87,12 +150,12 @@ const HeroWrapper = styled(Container)`
     flex-direction: column;
     align-items: center;
   }
-`;
+`
 
 const OutlinedButton = styled(Button)`
   border: 1px solid rgb(var(--textSecondary));
   color: rgb(var(--textSecondary));
-`;
+`
 
 const Contents = styled.div`
   flex: 1;
@@ -101,11 +164,11 @@ const Contents = styled.div`
   ${media('<=desktop')} {
     max-width: 100%;
   }
-`;
+`
 
 const CustomButtonGroup = styled(ButtonGroup)`
   margin-top: 4rem;
-`;
+`
 
 const ImageContainer = styled.div`
   display: flex;
@@ -124,7 +187,7 @@ const ImageContainer = styled.div`
       max-width: 80%;
     }
   }
-`;
+`
 
 const Description = styled.p`
   font-size: 1.8rem;
@@ -134,11 +197,11 @@ const Description = styled.p`
   ${media('<=desktop')} {
     font-size: 1.5rem;
   }
-`;
+`
 
 const CustomOverTitle = styled(OverTitle)`
   margin-bottom: 2rem;
-`;
+`
 
 const Heading = styled.h1`
   font-size: 6rem;
@@ -151,4 +214,4 @@ const Heading = styled.h1`
     font-size: 4.6rem;
     margin-bottom: 2rem;
   }
-`;
+`
